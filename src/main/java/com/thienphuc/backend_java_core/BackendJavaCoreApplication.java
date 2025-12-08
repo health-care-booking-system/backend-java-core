5ef52ad622a7abf0a21aa060ffd6354dab18e875
TenFukku <22521112@gm.uit.edu.vn>
2025-12-08 17:57:28 +0700

    chore: initial Spring Boot project setup

---
+++ src/main/java/com/thienphuc/backend_java_core/BackendJavaCoreApplication.java
@@ -0,0 +1,13 @@
+package com.thienphuc.backend_java_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendJavaCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendJavaCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thienphuc/backend_java_core/BackendJavaCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thienphuc.backend_java_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendJavaCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
