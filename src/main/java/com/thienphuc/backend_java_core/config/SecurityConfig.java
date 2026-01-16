6c44f9e391ae58888c1c149052ed6176c58f9390
TenFukku <22521112@gm.uit.edu.vn>
2026-01-16 11:22:26 +0700

    feat: implement jwt service

--- src/main/java/com/thienphuc/backend_java_core/config/SecurityConfig.java
+++ src/main/java/com/thienphuc/backend_java_core/config/SecurityConfig.java
@@ -1,12 +1,30 @@
 package com.thienphuc.backend_java_core.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    private final String[] PUBLIC_ENDPOINTS = {
+            "/users", "/auth/login", "/auth/introspect"
+    };
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity.authorizeHttpRequests(request -> request.requestMatchers(HttpMethod.POST, PUBLIC_ENDPOINTS)
+                .permitAll()
+                .anyRequest()
+                .authenticated())
+                .csrf(AbstractHttpConfigurer::disable);
+        return httpSecurity.build();
     }
 }
